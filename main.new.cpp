d5015fd1880fa8ef830b70db4015ce76e486e428
Fredrik Andersson <fredrikandersson@mac.com>
2022-05-16 11:16:00 +0200

    Added 3d rotation

---
+++ main.new.cpp
@@ -0,0 +1,151 @@
+#include <SDL/SDL.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
+#define WIDTH 800
+#define HEIGHT 600
+#define CENTER_X (WIDTH / 2.0)
+#define CENTER_Y (HEIGHT / 2.0)
+
+#define STARS 100000
+
+typedef struct {
+  float x, y, z;
+  float xv, yv, zv;
+} star;
+
+void update_stars(star *field, unsigned int size);
+
+#define RANGE 5
+#define RANGE_SCALE 1000.0
+
+float get_rand_range(float start, float end) {
+  int istart = start * RANGE_SCALE;
+  int iend = end * RANGE_SCALE;
+  return ((rand() % (int)(iend - istart)) + istart) / RANGE_SCALE;
+}
+star *init_starfield(unsigned int stars) {
+  star *field = (star *)malloc(sizeof(star) * stars);
+  for (int s = 0; s < stars; s++) {
+    field[s].x = get_rand_range(-RANGE / 2, RANGE / 2);
+    field[s].y = get_rand_range(-RANGE / 2, RANGE / 2);
+    field[s].z = get_rand_range(-RANGE / 2, RANGE / 2);
+    field[s].xv = (((rand() % 100) / 1000.0) / 5.0) + 0.01;
+    field[s].yv = 0.0; //(((rand()%100) / 1000.0)/5.0) + 0.01;
+    field[s].zv = 0.0; //(((rand()%100) / 1000.0)/5.0) + 0.01;
+  }
+  update_stars(field, stars);
+  return field;
+}
+
+float zA = 0.0, xA = 0.0, yA = 0.0;
+
+star rotate_star(star *the_star) {
+  star projected;
+  float x1 = the_star->x * cos(zA) - the_star->y * sin(zA);
+  float y1 = the_star->y * cos(zA) + the_star->x * sin(zA);
+  float z1 = the_star->z;
+
+  float x2 = x1 * cos(xA) - z1 * sin(xA);
+  float y2 = y1;
+  float z2 = z1 * cos(xA) + x1 * sin(xA);
+
+  float x3 = x2;
+  float y3 = y2 * cos(yA) - z2 * sin(yA);
+  float z3 = z2 * cos(yA) + y2 * sin(yA);
+
+  projected.x = x3;
+  projected.y = y3;
+  projected.z = z3;
+  return projected;
+}
+
+void draw_stars(SDL_Surface *screen, star *field, unsigned int size) {
+  xA += 0.001;
+  yA += 0.003;
+  zA += 0.005;
+
+  for (unsigned int s = 0; s < size; s++) {
+    star tmp = field[s];
+    tmp = rotate_star(&tmp);
+
+    float z = tmp.z;
+    float Cz = 0.004;
+    if (z > 1.0 && z <= 3.0) {
+      unsigned int x = (tmp.x * 400.0 / (Cz + z)) + CENTER_X;
+      unsigned int y = (tmp.y * 400.0 / (Cz + z)) + CENTER_Y;
+      if (x > 1 && x < WIDTH - 2 && y > 1 && y < HEIGHT - 2) {
+        unsigned char col = 0xff - (z * 50.0);
+        if (col > 1) {
+          if (*((Uint32 *)screen->pixels + y * WIDTH + x) < col) {
+            *((Uint32 *)screen->pixels + (y - 1) * WIDTH + x) =
+                SDL_MapRGBA(screen->format, col, col, col, 0xff);
+            *((Uint32 *)screen->pixels + y * WIDTH + (x + 1)) =
+                SDL_MapRGBA(screen->format, col, col, col, 0xff);
+            *((Uint32 *)screen->pixels + y * WIDTH + (x - 1)) =
+                SDL_MapRGBA(screen->format, col, col, col, 0xff);
+            *((Uint32 *)screen->pixels + y * WIDTH + x) =
+                SDL_MapRGBA(screen->format, col, col, col, 0xff);
+          }
+        }
+      }
+    }
+  }
+}
+
+float clamp_to_box(float value) {
+  float HR = (float)RANGE / 2.0;
+
+  if (value < -HR) {
+    return HR;
+  }
+  if (value > HR) {
+    return -HR;
+  }
+  return value;
+}
+
+void update_stars(star *field, unsigned int size) {
+
+  for (unsigned int s = 0; s < size; s++) {
+    field[s].z -= field[s].zv;
+    field[s].z = clamp_to_box(field[s].z);
+
+    field[s].x -= field[s].xv;
+    field[s].x = clamp_to_box(field[s].x);
+
+    field[s].y -= field[s].yv;
+    field[s].y = clamp_to_box(field[s].y);
+  }
+}
+
+extern "C" int main(int argc, char **argv) {
+  srand(time(NULL));
+
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Surface *screen = SDL_SetVideoMode(WIDTH, HEIGHT, 32, SDL_SWSURFACE);
+  star *field = init_starfield(STARS);
+
+  while (true) {
+    if (SDL_MUSTLOCK(screen))
+      SDL_LockSurface(screen);
+    memset(screen->pixels, 0, WIDTH * HEIGHT * 4);
+
+    update_stars(field, STARS);
+    draw_stars(screen, field, STARS);
+    if (SDL_MUSTLOCK(screen))
+      SDL_UnlockSurface(screen);
+    SDL_Flip(screen);
+    emscripten_sleep(1);
+  }
+
+  SDL_Quit();
+
+  return 0;
+}
---
+++ main.old.cpp
@@ -0,0 +1,106 @@
+#include <SDL/SDL.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
+#define WIDTH 640
+#define HEIGHT 480
+#define CENTER_X (WIDTH / 2.0)
+#define CENTER_Y (HEIGHT / 2.0)
+
+#define STARS 100000
+
+#define SPEED_X 5
+#define SPEED_Y 5
+#define SPEED_Z 5
+
+typedef struct {
+  float x, y, z;
+  float xv, yv, zv;
+} star;
+
+void update_stars(star *field, unsigned int size);
+
+star *init_starfield(unsigned int stars) {
+  star *field = (star *)malloc(sizeof(star) * stars);
+  for (int s = 0; s < stars; s++) {
+    unsigned int iz = (rand() % 3000);
+    float z = (iz / 1000.0) + 1.;
+    field[s].x = (rand() % lround(WIDTH * z) * 2.) - (WIDTH * z);
+    field[s].y = (rand() % lround(HEIGHT * z) * 2.) - (HEIGHT * z);
+    field[s].z = z + 1.0;
+    field[s].xv = 1.0 + ((rand() % (SPEED_X * 10)) / 10.0);
+    field[s].yv = 0.0; 
+    field[s].zv = 0.01;
+  }
+  update_stars(field, stars);
+  return field;
+}
+
+void draw_stars(SDL_Surface *screen, star *field, unsigned int size) {
+  for (unsigned int s = 0; s < size; s++) {
+    float z = field[s].z;
+
+    unsigned int x = (field[s].x / z) + CENTER_X;
+    unsigned int y = (field[s].y / z) + CENTER_Y;
+    if (x > 1 && x < WIDTH - 2 && y > 1 && y < HEIGHT - 2) {
+      unsigned char col = 0xff - ((0xff / 3) * (z - 1.0));
+      if (col > 10) {
+        if (*((Uint32 *)screen->pixels + y * WIDTH + x) < col) {
+          *((Uint32 *)screen->pixels + (y - 1) * WIDTH + x) =
+              SDL_MapRGBA(screen->format, col, col, col, 0xff);
+          *((Uint32 *)screen->pixels + y * WIDTH + (x + 1)) =
+              SDL_MapRGBA(screen->format, col, col, col, 0xff);
+          *((Uint32 *)screen->pixels + y * WIDTH + (x - 1)) =
+              SDL_MapRGBA(screen->format, col, col, col, 0xff);
+          *((Uint32 *)screen->pixels + y * WIDTH + x) =
+              SDL_MapRGBA(screen->format, col, col, col, 0xff);
+        }
+      }
+    }
+  }
+}
+
+void update_stars(star *field, unsigned int size) {
+  for (unsigned int s = 0; s < size; s++) {
+    field[s].z -= field[s].zv;
+    if (field[s].z < 1.0) {
+      field[s].z = 4.;
+    }
+    field[s].x -= field[s].xv;
+    if (field[s].x < -2000.0) {
+      field[s].x = 2000.0; // WIDTH*z;
+    }
+    field[s].y -= field[s].yv;
+    if (field[s].y < -2000.0) {
+      field[s].y = 2000.0;
+    }
+  }
+}
+
+extern "C" int main(int argc, char **argv) {
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Surface *screen = SDL_SetVideoMode(WIDTH, HEIGHT, 32, SDL_SWSURFACE);
+  star *field = init_starfield(STARS);
+
+  while (true) {
+    if (SDL_MUSTLOCK(screen))
+      SDL_LockSurface(screen);
+    memset(screen->pixels, 0, WIDTH * HEIGHT * 4);
+
+    update_stars(field, STARS);
+    draw_stars(screen, field, STARS);
+    if (SDL_MUSTLOCK(screen))
+      SDL_UnlockSurface(screen);
+    SDL_Flip(screen);
+    emscripten_sleep(16);
+  }
+
+  SDL_Quit();
+
+  return 0;
+}
