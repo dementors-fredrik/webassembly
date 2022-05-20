0b499d1a4d6956c6dbc5846e57f09393bc8e456f
Fredrik Andersson <fredrikandersson@mac.com>
2022-05-20 05:15:21 +0200

    Experimental branch code

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CXX = em++
-CXXFLAGS = -Wall -O3 -sASYNCIFY
+CXXFLAGS = -Wall -O3 -sASYNCIFY -flto -fno-rtti -fno-exceptions --closure 1 -sENVIRONMENT=web,worker -s MODULARIZE=1  -s 'EXPORT_NAME="createStarfield"'
 
 PYTHON = python3
 PYTHON_ARGS = -m http.server
@@ -9,10 +9,12 @@ SOURCES = main.cpp
 MAIN = emout
 
 $(MAIN): 
-	$(CXX) $(CXXFLAGS) $(SOURCES) -o dist/$(MAIN).html
+	$(CXX) $(CXXFLAGS) $(SOURCES) --shell-file resources/shell.html -o dist/$(MAIN).html
 
 clean:
 	$(RM) dist/*
+base64: $(MAIN)
+	base64 dist/emout.wasm > dist/emout.b64
 
 start: clean $(MAIN)
 	emrun dist/$(MAIN).html
--- main.new.cpp
+++ main.new.cpp
@@ -13,7 +13,9 @@
 #define CENTER_X (WIDTH / 2.0)
 #define CENTER_Y (HEIGHT / 2.0)
 
-#define STARS 20000
+#define INTERNAL_SPEED 20
+
+#define STARS 3000
 
 int SUBF = 4;
 
@@ -23,15 +25,28 @@ typedef struct {
   float size;
 } star;
 
+
+float compare_with_tresh(float a, float b, float tresh) {
+  if(a > b-tresh && a < b+tresh ) {
+    return true;
+  }
+  return false;
+}
+
 void update_stars(star *field, unsigned int size);
 
-#define RANGE 10
+#define RANGE 20
 #define RANGE_SCALE 1000.0
 
 float *zbuffer;
+Uint8 *cbuffer;
 
 float *init_zbuffer() {
   return (float *)malloc(WIDTH * HEIGHT * sizeof(float));
+}
+
+Uint8 *init_cbuffer() {
+  return (Uint8*)malloc(WIDTH * HEIGHT * sizeof(Uint8));
 }
 
 void clear_zbuffer() {
@@ -40,11 +55,29 @@ void clear_zbuffer() {
   }
 }
 
+void clear_cbuffer() {
+  for(int j=0;j<WIDTH*HEIGHT;j++) {
+    cbuffer[j] = 0;
+  }
+}
+
+void interpolate_z_from_cbuffer() {
+  for(int j=1;j<(WIDTH*HEIGHT)-1;j++) {
+    float delta = cbuffer[j-1] - cbuffer[j];
+    if(cbuffer[j]>0) {
+      zbuffer[j]+= delta > 0 ? 0.1 : -0.1;
+    } else {
+      zbuffer[j]=20.0;
+    }
+  }
+}
+
 float get_rand_range(float start, float end) {
   int istart = start * RANGE_SCALE;
   int iend = end * RANGE_SCALE;
   return ((rand() % (int)(iend - istart)) + istart) / RANGE_SCALE;
 }
+
 star *init_starfield(unsigned int stars) {
   star *field = (star *)malloc(sizeof(star) * stars);
   for (int s = 0; s < stars; s++) {
@@ -53,16 +86,24 @@ star *init_starfield(unsigned int stars) {
     field[s].z = get_rand_range(-RANGE / 2, RANGE / 2);
     //      field[s].y = sin(s/1000.0)*cos(s/20.0) * 10.0;
     //      field[s].z = cos(s/1000.0)*sin(s/20.0) * 10.0;
-    field[s].xv = get_rand_range(0.005, 0.10); //(((rand() % 100) / 1000.0) / 5.0) + 0.01;
-    field[s].yv = get_rand_range(-0.005, 0.005);
-    field[s].zv = get_rand_range(-0.005, 0.005);
-    field[s].size = get_rand_range(3, 25);
+    field[s].xv = get_rand_range(0.004, 0.01); //(((rand() % 100) / 1000.0) / 5.0) + 0.01;
+    field[s].yv = 0;//get_rand_range(-0.005, 0.005);
+    field[s].zv = 0;//get_rand_range(-0.005, 0.005);
+    field[s].size = get_rand_range(1, 5);;
+    if(rand()%100 < 5) {
+      field[s].size = get_rand_range(15, 100);
+    }
+    
+   
   }
   update_stars(field, stars);
   return field;
 }
 
-float zA = 0.0, xA = 1.5, yA = .0;
+float zA = 1.3, xA = 1.5, yA = 1.2, FOV = 25.0;
+float targetZAng=1.3, targetXAng = 1.5, targetYAng=1.2, targetFOV = 25.0;
+int nextSeed = 100*INTERNAL_SPEED;
+
 
 star rotate_star(star *the_star) {
   star projected;
@@ -85,56 +126,61 @@ star rotate_star(star *the_star) {
   return projected;
 }
 
-int nextSeed = 1000;
-float targetZAng, targetXAng = 1.5, targetYAng;
 
 #define PI 3.14159256
 
 void rotate_camera() {
   if (nextSeed <= 0) {
-    if (SUBF < 16) {
-
-      SUBF *= 2;
-    }
-    unsigned char rotateBits = rand() % 7;
+    unsigned char rotateBits = rand() % 16;
     if (rotateBits & 0x1) {
-      targetZAng = get_rand_range(-180 * (PI / 180.0), 180 * (PI / 180.0));
+      targetZAng = get_rand_range(-180 * (PI / 180.0), 360 * (PI / 180.0));
     }
     if (rotateBits & 0x2) {
-      targetXAng = get_rand_range(-180 * (PI / 180.0), 180 * (PI / 180.0));
+      targetXAng = get_rand_range(-180 * (PI / 180.0), 360 * (PI / 180.0));
     }
     if (rotateBits & 0x4) {
-      targetYAng = get_rand_range(-180 * (PI / 180.0), 180 * (PI / 180.0));
+      targetYAng = get_rand_range(-180 * (PI / 180.0), 360 * (PI / 180.0));
     }
-    nextSeed = 400 + rand() % 1200;
+    //if (rotateBits & 0x8 && compare_with_tresh(FOV, targetFOV, 1.0)) {
+      if(targetFOV > 300.0) {
+       targetFOV = get_rand_range(25.0,50.0);        
+      } else {
+       targetFOV = get_rand_range(150.0,1000.0);                
+      }
+    //}
+    nextSeed = (400*INTERNAL_SPEED) + (rand() % 1200)*INTERNAL_SPEED;
   }
   nextSeed--;
+  
+  FOV+=(targetFOV-FOV)/((float)INTERNAL_SPEED*100.);
+  
+
   if (xA < targetXAng) {
-    xA += 0.005;
+    xA += 0.005/(float)INTERNAL_SPEED;
   }
 
   if (xA > targetXAng) {
-    xA -= 0.005;
+    xA -= 0.005/(float)INTERNAL_SPEED;
   }
 
   if (yA < targetYAng) {
-    yA += 0.005;
+    yA += 0.005/(float)INTERNAL_SPEED;
   }
 
   if (yA > targetYAng) {
-    yA -= 0.005;
+    yA -= 0.005/(float)INTERNAL_SPEED;
   }
 
   if (zA < targetZAng) {
-    zA += 0.005;
+    zA += 0.005/(float)INTERNAL_SPEED;
   }
 
   if (zA > targetZAng) {
-    zA -= 0.005;
+    zA -= 0.005/(float)INTERNAL_SPEED;
   }
 }
 
-#define FOV (300.0)
+//#define FOV (300.0)
 
 inline void draw_pixel(SDL_Surface *screen, float x, float y, float z,
                        Uint32 color) {
@@ -147,15 +193,15 @@ inline void draw_pixel(SDL_Surface *screen, float x, float y, float z,
 }
 
 void draw_filled_circle(SDL_Surface *screen, float x, float y, float z, int r) {
-  float zBase = z * 20.0;
+  float zBase = z * 15.0;
   for (int i = 0; i < r * 2; i++)
     for (int j = 0; j < r * 2; j++) {
       float d = (float)sqrt((i - r) * (i - r) + (j - r) * (j - r));
       if (d < r) {
-        Uint32 color = SDL_MapRGBA(screen->format, (0x88 - (d)) - zBase,
-                                   (0x44 - (d / 2)) - zBase,
-                                   (0x33 - (d * 4)) - zBase, 0xff);
-        draw_pixel(screen, x + i - (r / 2), y + j - (r / 2), z, color);
+        Uint32 color = SDL_MapRGBA(screen->format, (0x99 - (d)) - zBase,
+                                   (0x99 - (d / 2)) - zBase,
+                                   (0xBB - (d * 3)) - zBase, 0xff);
+        draw_pixel(screen, x + i - (r / 2), y + j - (r / 2), z-(d*2), color);
       }
     }
 }
@@ -173,9 +219,24 @@ void draw_shaded_circle(SDL_Surface *screen, float x, float y, float z, float r,
 
 float wave = 0.0;
 
+
+
+int compare( const void* a, const void* b)
+{
+     star as = * ( (star*) a );
+     star bs = * ( (star*) b );
+
+     if ( compare_with_tresh(as.z, bs.z,0.0001 )) return 0;
+     else if ( as.z < bs.z ) return -1;
+     else return 1;
+}
+
 void draw_stars(SDL_Surface *screen, star *field, unsigned int size) {
 
   rotate_camera();
+
+
+  qsort( field, 6, sizeof(int), compare );
 
   for (unsigned int s = 0; s < size; s++) {
     star tmp = field[s];
@@ -183,13 +244,25 @@ void draw_stars(SDL_Surface *screen, star *field, unsigned int size) {
     float z = tmp.z;
 
     float Cz = 0.004;
-    if (z > 0.01 && z <= 4.0) {
+    float fovScale = FOV/1000.0;
+
+    if (z > 0.5*fovScale && z <= 5.0*fovScale*20.0) {
       unsigned int x = (tmp.x * FOV / (Cz + z)) + CENTER_X;
       unsigned int y = (tmp.y * FOV / (Cz + z)) + CENTER_Y;
 
       if (x > 0 && x < WIDTH && y > 0 && y < HEIGHT) {
-        draw_filled_circle(screen, x, y, tmp.z, tmp.size);
+        float size = fovScale * (tmp.size/(z*0.8));
+        if(size > 1. && size < 150.) {
+          draw_filled_circle(screen, x, y, tmp.z, size);
+        } else {
+           float zBase = z * 10.0;
+          Uint32 color = SDL_MapRGBA(screen->format, (0x77 - (zBase)),
+                                   (0x66 - (zBase / 2)),
+                                   (0x99 - (zBase * 3)) , 0xff);
+          draw_pixel(screen, x  , y  , z, color);
+        }
       }
+
     }
   }
 }
@@ -219,6 +292,33 @@ void update_stars(star *field, unsigned int size) {
   }
 }
 
+inline Uint8 fade_value(Uint8 v, Uint8 speed) {
+  const Uint8 fade_speed = speed;
+  if(v-fade_speed>0) {
+    return v-fade_speed;
+  } else if(v>0) {
+    return v-1;
+  }
+  return v;
+}
+
+void blur_fade(SDL_Surface *screen) {
+  for(int y=0;y<HEIGHT;y++) {
+    for(int x=0;x<WIDTH;x++) {
+      Uint32 *pixel = &((Uint32*)screen->pixels)[y*WIDTH+x];
+      Uint8 r,g,b,a;
+      SDL_GetRGBA(*pixel, screen->format, &r, &g, &b, &a);
+      
+      r=fade_value(r,32);
+      g=fade_value(g,32);
+      b=fade_value(b,18);
+      a=fade_value(a,16);
+
+      *pixel = SDL_MapRGBA(screen->format, r,g,b,a);
+    }
+  }
+}
+
 extern "C" int main(int argc, char **argv) {
   srand(time(NULL));
 
@@ -226,18 +326,27 @@ extern "C" int main(int argc, char **argv) {
   SDL_Surface *screen = SDL_SetVideoMode(WIDTH, HEIGHT, 32, SDL_SWSURFACE);
   star *field = init_starfield(STARS);
   zbuffer = init_zbuffer();
+  cbuffer = init_cbuffer();
+  clear_zbuffer();
+  clear_cbuffer();
 
   while (true) {
     if (SDL_MUSTLOCK(screen))
       SDL_LockSurface(screen);
     clear_zbuffer();
-    memset(screen->pixels, 0, WIDTH * HEIGHT * 4);
-    update_stars(field, STARS);
-    draw_stars(screen, field, STARS);
+
+  //  memset(screen->pixels, 0, WIDTH * HEIGHT * 4);
+      blur_fade(screen);
+
+    for(int speed=0;speed<INTERNAL_SPEED;speed++) {
+      update_stars(field, STARS);
+      draw_stars(screen, field, STARS);
+    }
+    
     if (SDL_MUSTLOCK(screen))
       SDL_UnlockSurface(screen);
     SDL_Flip(screen);
-    emscripten_sleep(1);
+    emscripten_sleep(16);
   }
 
   SDL_Quit();
